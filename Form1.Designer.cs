13e44817415e2f78f99899393f9c8d3d56b62ef2
yngryzen <175751726+yngryzen@users.noreply.github.com>
2024-09-02 10:46:08 +0100

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace CheatChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.discordinput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cheateravatar = new System.Windows.Forms.PictureBox();
+            this.cheatername = new System.Windows.Forms.Label();
+            this.cheatsused = new System.Windows.Forms.Label();
+            this.cheaterdate = new System.Windows.Forms.Label();
+            this.notcheater = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.discordegg = new System.Windows.Forms.Label();
+            this.invalidid = new System.Windows.Forms.Label();
+            this.Alts = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.cheateravatar)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Purple;
+            this.label1.Location = new System.Drawing.Point(4, 133);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Discord ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // discordinput
+            // 
+            this.discordinput.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.discordinput.Location = new System.Drawing.Point(4, 158);
+            this.discordinput.Margin = new System.Windows.Forms.Padding(4);
+            this.discordinput.Name = "discordinput";
+            this.discordinput.Size = new System.Drawing.Size(240, 22);
+            this.discordinput.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Thistle;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Purple;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(246, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Check";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cheateravatar
+            // 
+            this.cheateravatar.ErrorImage = ((System.Drawing.Image)(resources.GetObject("cheateravatar.ErrorImage")));
+            this.cheateravatar.Image = ((System.Drawing.Image)(resources.GetObject("cheateravatar.Image")));
+            this.cheateravatar.InitialImage = ((System.Drawing.Image)(resources.GetObject("cheateravatar.InitialImage")));
+            this.cheateravatar.Location = new System.Drawing.Point(-1, 0);
+            this.cheateravatar.Name = "cheateravatar";
+            this.cheateravatar.Size = new System.Drawing.Size(96, 99);
+            this.cheateravatar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cheateravatar.TabIndex = 3;
+            this.cheateravatar.TabStop = false;
+            this.cheateravatar.WaitOnLoad = true;
+            this.cheateravatar.Click += new System.EventHandler(this.cheateravatar_Click);
+            // 
+            // cheatername
+            // 
+            this.cheatername.AutoSize = true;
+            this.cheatername.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cheatername.ForeColor = System.Drawing.Color.Purple;
+            this.cheatername.Location = new System.Drawing.Point(101, 8);
+            this.cheatername.Name = "cheatername";
+            this.cheatername.Size = new System.Drawing.Size(73, 28);
+            this.cheatername.TabIndex = 4;
+            this.cheatername.Text = "Name";
+            this.cheatername.Visible = false;
+            // 
+            // cheatsused
+            // 
+            this.cheatsused.AutoSize = true;
+            this.cheatsused.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cheatsused.ForeColor = System.Drawing.Color.Purple;
+            this.cheatsused.Location = new System.Drawing.Point(101, 36);
+            this.cheatsused.Name = "cheatsused";
+            this.cheatsused.Size = new System.Drawing.Size(109, 21);
+            this.cheatsused.TabIndex = 5;
+            this.cheatsused.Text = "Cheats Used:";
+            this.cheatsused.Visible = false;
+            // 
+            // cheaterdate
+            // 
+            this.cheaterdate.AutoSize = true;
+            this.cheaterdate.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cheaterdate.ForeColor = System.Drawing.Color.Purple;
+            this.cheaterdate.Location = new System.Drawing.Point(101, 57);
+            this.cheaterdate.Name = "cheaterdate";
+            this.cheaterdate.Size = new System.Drawing.Size(113, 21);
+            this.cheaterdate.TabIndex = 6;
+            this.cheaterdate.Text = "Last Cheated:";
+            this.cheaterdate.Visible = false;
+            // 
+            // notcheater
+            // 
+            this.notcheater.AutoSize = true;
+            this.notcheater.BackColor = System.Drawing.Color.Thistle;
+            this.notcheater.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.notcheater.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.notcheater.Location = new System.Drawing.Point(100, 8);
+            this.notcheater.Name = "notcheater";
+            this.notcheater.Size = new System.Drawing.Size(153, 28);
+            this.notcheater.TabIndex = 7;
+            this.notcheater.Text = "Not a Cheater";
+            this.notcheater.Visible = false;
+            this.notcheater.Click += new System.EventHandler(this.notcheater_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(393, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 21);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Made by Ryzenix";
+            // 
+            // discordegg
+            // 
+            this.discordegg.AutoSize = true;
+            this.discordegg.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.discordegg.Location = new System.Drawing.Point(337, 128);
+            this.discordegg.Name = "discordegg";
+            this.discordegg.Size = new System.Drawing.Size(191, 28);
+            this.discordegg.TabIndex = 10;
+            this.discordegg.Text = "Discord: yngryzen";
+            // 
+            // invalidid
+            // 
+            this.invalidid.AutoSize = true;
+            this.invalidid.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.invalidid.ForeColor = System.Drawing.Color.Red;
+            this.invalidid.Location = new System.Drawing.Point(3, 105);
+            this.invalidid.Name = "invalidid";
+            this.invalidid.Size = new System.Drawing.Size(207, 28);
+            this.invalidid.TabIndex = 11;
+            this.invalidid.Text = "ERROR: INVALID ID";
+            this.invalidid.Visible = false;
+            // 
+            // Alts
+            // 
+            this.Alts.AutoSize = true;
+            this.Alts.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Alts.Location = new System.Drawing.Point(101, 77);
+            this.Alts.Name = "Alts";
+            this.Alts.Size = new System.Drawing.Size(147, 22);
+            this.Alts.TabIndex = 12;
+            this.Alts.Text = "Has alt accounts.";
+            this.Alts.Visible = false;
+            this.Alts.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.invalidid);
+            this.panel1.Controls.Add(this.discordegg);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.Alts);
+            this.panel1.Controls.Add(this.notcheater);
+            this.panel1.Controls.Add(this.cheaterdate);
+            this.panel1.Controls.Add(this.cheatsused);
+            this.panel1.Controls.Add(this.cheatername);
+            this.panel1.Controls.Add(this.cheateravatar);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.discordinput);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(531, 185);
+            this.panel1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(529, 182);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.99D;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CheatChecker";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cheateravatar)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox discordinput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox cheateravatar;
+        private System.Windows.Forms.Label cheatername;
+        private System.Windows.Forms.Label cheatsused;
+        private System.Windows.Forms.Label cheaterdate;
+        private System.Windows.Forms.Label notcheater;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label discordegg;
+        private System.Windows.Forms.Label invalidid;
+        private System.Windows.Forms.Label Alts;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CheatChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Cursor = Cursors.WaitCursor;
+            string DiscordID = discordinput.Text;
+            string EndPoint = "https://inf.sh/amongus/cheaters.json";
+            string json = "";
+            Alts.Visible = false;
+            invalidid.Visible = false;
+            if (System.IO.File.Exists("CheaterList.json") && System.IO.File.GetLastWriteTime("CheaterList.json") >= DateTime.Now.AddMinutes(-240)) // use old file
+            {
+                json = System.IO.File.ReadAllText("CheaterList.json");
+            }
+            else // Make and use new file.
+            {
+                WebResponse Response = WebRequest.Create(EndPoint).GetResponse();
+                json = new StreamReader(Response.GetResponseStream()).ReadToEnd();
+                System.IO.File.WriteAllText("CheaterList.json", json);
+            }
+            JArray Cheaters = JArray.Parse(json);
+            foreach (var Cheater in Cheaters)
+            {
+                if ((string)Cheater["discord_id"] == DiscordID) // Compares DiscordID to Discord ID's via inf.sh/amongus/cheaters.json
+                { // Sets information to the cheaters
+                    cheatername.Text = (string)Cheater["discord_tag"];
+                    cheatername.Visible = true;
+                    Alts.Text = (string)Cheater["alt_of"];
+                    if (Alts.Text != "")
+                    {
+                        Alts.Visible = true;
+                        Alts.Text = "Is an alt account";
+                    }
+                    cheateravatar.Load((string)Cheater["avatar_url"]);
+                    cheateravatar.Visible = true;
+                    cheatsused.Text = "Cheats Used: " + (string)Cheater["cheats_used"];
+                    cheatsused.Visible = true;
+                    cheaterdate.Text = "Last Cheated: " + (string)Cheater["last_cheated"];
+                    cheaterdate.Visible = true;
+                    notcheater.Visible = false;
+                    invalidid.Visible = false;
+                    discordinput.ResetText();
+                    break;
+                }
+                else if (DiscordID.Length < 16) // Invalid ID
+                {
+                    invalidid.Visible = true;
+                    cheateravatar.Image = cheateravatar.ErrorImage;
+                    discordinput.ResetText();
+                    cheatername.Visible = false;
+                    Alts.Visible = false;
+                    cheatsused.Visible = false;
+                    cheaterdate.Visible = false;
+                    notcheater.Visible = false;
+                }
+                else if ((string)Cheater["discord_id"] != DiscordID) // No cheater found.
+                {
+                    cheatername.Visible = false;
+                    cheateravatar.Visible = true;
+                    cheateravatar.Image = cheateravatar.InitialImage;
+                    cheatsused.Visible = false;
+                    cheaterdate.Visible = false;
+                    notcheater.Visible = true;
+                    discordinput.ResetText();
+                }
+            }
+            panel1.Cursor = Cursors.Default;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void notcheater_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cheateravatar_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+    }
+}
